25aa60583d66415077afc056463c7b1e8f668857
asteraceae <dreyes5@luc.edu>
2022-03-14 19:19:12 -0500

    removed redundancy

--- main.py
+++ main.py
@@ -1,34 +1,32 @@
-import os
-os.add_dll_directory("C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v11.6/bin")
-
 import random
-import tensorflow as tf
 import pickle
 from datetime import datetime
-from tensorflow.keras.layers import Dense, GlobalAveragePooling2D, Convolution2D, BatchNormalization, Flatten , MaxPooling2D, Dropout
+from tensorflow import divide
+from tensorflow.keras import Input
+from tensorflow.keras.layers import Dense, Lambda, GlobalAveragePooling2D, Convolution2D, BatchNormalization, Flatten , MaxPooling2D, Dropout
 from tensorflow.keras.preprocessing import image
-from tensorflow.keras.applications.densenet import preprocess_input
-from tensorflow.keras.applications import DenseNet169
-from tensorflow.keras.utils import image_dataset_from_directory
+from tensorflow.keras.applications.resnet50 import preprocess_input
+from keras.applications.densenet import DenseNet169
+from tensorflow.keras.utils import image_dataset_from_directory, plot_model
 from tensorflow.keras.models import Model
 from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.callbacks import ModelCheckpoint, ReduceLROnPlateau
+from tensorflow.keras.initializers import he_normal
 
 def get_architecture_dataset(directory, image_height, image_width, batch_size):
      seed = random.getrandbits(32)
 
-     train = tf.keras.utils.image_dataset_from_directory(
+     train = image_dataset_from_directory(
     directory, labels='inferred', label_mode='int',
     class_names=None, color_mode='rgb', batch_size=batch_size, image_size=(image_height,
     image_width), shuffle=True, seed=seed, validation_split=0.2, subset='training')
  
-     test = tf.keras.utils.image_dataset_from_directory(
+     test = image_dataset_from_directory(
     directory, labels='inferred', label_mode='int',
     class_names=None, color_mode='rgb', batch_size=batch_size, image_size=(image_height,
     image_width), shuffle=True, seed=seed, validation_split=0.2, subset='validation')
 
-     train = train.map(lambda x, y: (tf.divide(x, 255), y))
-     test = test.map(lambda x, y: (tf.divide(x, 255), y))
+     train = train.map(lambda x, y: (divide(x, 255), y))
+     test = test.map(lambda x, y: (divide(x, 255), y))
 
      return train, test
 
@@ -48,29 +46,29 @@ def densenet(image_height, image_width, channels):
         else:
             layer.trainable = False
 
-    input = tf.keras.Input(shape = (image_height, image_width, channels))
-    preprocess = tf.keras.layers.Lambda(lambda x: tf.image.resize(x, (224, 224)), name='lamb')(input)
-    initializer = tf.keras.initializers.he_normal(seed = 32)
+    input = Input(shape = (image_height, image_width, channels))
+    preprocess = Lambda(lambda x: image.resize(x, (224, 224)), name='lamb')(input)
+    initializer = he_normal(seed = 32)
 
     layer = dmodel(inputs = preprocess)
-    layer = tf.keras.layers.Flatten()(layer)
-    layer = tf.keras.layers.BatchNormalization()(layer)
-    layer = tf.keras.layers.Dense(units = 256,
+    layer = Flatten()(layer)
+    layer = BatchNormalization()(layer)
+    layer = Dense(units = 256,
                         activation = 'relu',
                         kernel_initializer = initializer
                         )(layer)
-    layer = tf.keras.layers.Dropout(0.4)(layer)
-    layer = tf.keras.layers.BatchNormalization()(layer)
-    layer = tf.keras.layers.Dense(units = 128,
+    layer = Dropout(0.4)(layer)
+    layer = BatchNormalization()(layer)
+    layer = Dense(units = 128,
                         activation = 'relu',
                         kernel_initializer = initializer
                         )(layer)
-    layer = tf.keras.layers.Dropout(0.4)(layer)
-    layer = tf.keras.layers.Dense(units = 25,
+    layer = Dropout(0.4)(layer)
+    layer = Dense(units = 25,
                         activation = 'softmax',
                         kernel_initializer = initializer
                         )(layer)
-    model = tf.keras.models.Model(inputs = input, outputs = layer)
+    model = Model(inputs = input, outputs = layer)
     return model
 
 
@@ -99,5 +97,5 @@ if __name__ == "__main__":
     pickle.dump(model, outfile)
     outfile.close()
 
-    tf.keras.utils.plot_model(model, show_shapes = True)
+    plot_model(model, show_shapes = True)
 
